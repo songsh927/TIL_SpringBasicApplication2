a98e86bf58a5705557530a52e5b1bf09b068cfe8
songseunghyun <songsh96927@gmail.com>
2022-03-03 23:04:39 +0900

    22.03.03

--- src/main/java/hello/core/AppConfig.java
+++ src/main/java/hello/core/AppConfig.java
@@ -31,6 +31,7 @@ public class AppConfig {
     public OrderService orderService(){
         System.out.println("AppConfig.orderService");
         return new OrderServiceImpl( memberRepository(), discountPolicy());
+        //return null;
     }
 
     @Bean
