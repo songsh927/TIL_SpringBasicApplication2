cc284763c84b2059d1591195a30b384d6c806fa2
songseunghyun <songsh96927@gmail.com>
2022-04-16 23:15:37 +0900

    22.04.16

--- src/test/java/hello/core/lifecycle/NetworkClient.java
+++ src/test/java/hello/core/lifecycle/NetworkClient.java
@@ -1,12 +1,14 @@
 package hello.core.lifecycle;
 
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
+
 public class NetworkClient {
     private String url;
 
     public NetworkClient() {
         System.out.println("생성자 호출, url = " + url);
-        connect();
-        call("초기화 연결 메시지");
+
     }
 
     public void setUrl(String url) {
@@ -24,6 +26,19 @@ public class NetworkClient {
 
     //서비스 종료시 호출
     public void disconnect(){
-        System.out.println("close" + url);
+        System.out.println("close: " + url);
+    }
+
+    @PostConstruct
+    public void init() {
+        System.out.println("NetworkClient.init");
+        connect();
+        call("초기화 연결 메시지");
+    }
+
+    @PreDestroy
+    public void close(){
+        System.out.println("NetworkClient.close");
+        disconnect();
     }
 }
